b0f560d2ab010f398d213c83d2bbbc1dff48267c
hhllhhyyds <hhllhhyyds@sina.com>
2024-03-04 10:48:46 +0800

    Fix ping pong send receive demo when communicator size is larger than 2

---
+++ send_recv/basic.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void print_mpi_basic_infos();
--- send_recv/send_recv_2.cpp
+++ send_recv/send_recv_2.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define PING_PONG_LIMIT 11
+#include "basic.h"
 
-void print_mpi_basic_infos();
+#define PING_PONG_LIMIT 11
 
 int main(int argc, char **argv)
 {
@@ -27,34 +27,18 @@ int main(int argc, char **argv)
             ping_pong_count++;
             MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
         }
-        else
+        else if (world_rank == (ping_pong_count + 1) % 2)
         {
             MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("Process %d received ping pang count %d from process %d\n", world_rank, ping_pong_count, partner_rank);
-            // sleep(1);
+            sleep(1);
+        }
+        else
+        {
+            break;
         }
     }
 
     // Finalize the MPI environment.
     MPI_Finalize();
-}
-
-void print_mpi_basic_infos()
-{
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print off a hello world message
-    printf("Running in processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
 }
